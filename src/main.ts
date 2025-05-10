b22e33c26e09e59a39ce23920143153d21de24f2
baxtiyorqurbonnazarov33 <baxtiyorqurbonnazarov33@gmail.com>
2025-05-10 23:45:39 +0500

    feat: update CORS configuration to allow access from the admin panel URL

--- src/main.ts
+++ src/main.ts
@@ -23,7 +23,7 @@ async function bootstrap() {
       'https://kelishamiz-backend.vercel.app',
       'https://it-experts-nine.vercel.app',
       'http://localhost:5174',
-      "https://kelishamiz-admin-panels.vercel.app/auth/login"
+      "https://kelishamiz-admin-panels.vercel.app"
     ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
