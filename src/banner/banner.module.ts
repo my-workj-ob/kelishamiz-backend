ba48b87ff7b4540299d33da80f82d2bff9fcfb61
baxtiyorqurbonnazarov33 <baxtiyorqurbonnazarov33@gmail.com>
2025-06-03 15:47:54 +0500

    feat(banner): add FileService and FileEntity to BannerModule imports and providers

--- src/banner/banner.module.ts
+++ src/banner/banner.module.ts
@@ -4,13 +4,15 @@ import { BannerController } from './banner.controller';
 import { BannerService } from './banner.service';
 import { Banner } from './entities/banner.entity';
 import { UploadService } from './../file/uploadService';
+import { FileService } from './../file/file.service';
+import { FileEntity } from './../file/entities/file.entity';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Banner]), // <-- Buni qo'shing. Banner entitysini shu modulga ta'minlaydi.
+    TypeOrmModule.forFeature([Banner, FileEntity]), // <-- Buni qo'shing. Banner entitysini shu modulga ta'minlaydi.
   ],
   controllers: [BannerController],
-  providers: [BannerService, UploadService],
+  providers: [BannerService, UploadService, FileService],
   // Agar boshqa modullar BannerService dan foydalanishi kerak bo'lsa,
   // BannerService ni export qilishni unutmang:
   // exports: [BannerService],
