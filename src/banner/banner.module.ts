7f30b070aacdfe3f6d490bf466be2354a611ab0f
baxtiyorqurbonnazarov33 <baxtiyorqurbonnazarov33@gmail.com>
2025-06-03 15:33:52 +0500

    feat(banner): update providers in BannerModule to use UploadService instead of CloudinaryService

--- src/banner/banner.module.ts
+++ src/banner/banner.module.ts
@@ -4,13 +4,14 @@ import { BannerController } from './banner.controller';
 import { BannerService } from './banner.service';
 import { Banner } from './entities/banner.entity'; // <-- Banner entitysini qo'shing
 import { CloudinaryService } from './cloudinary.service';
+import { UploadService } from './../file/uploadService';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([Banner]), // <-- Buni qo'shing. Banner entitysini shu modulga ta'minlaydi.
   ],
   controllers: [BannerController],
-  providers: [BannerService, CloudinaryService],
+  providers: [BannerService, UploadService],
   // Agar boshqa modullar BannerService dan foydalanishi kerak bo'lsa,
   // BannerService ni export qilishni unutmang:
   // exports: [BannerService],
