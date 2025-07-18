163766aa030b9eab013429d27a2a3d2f486b392a
baxtiyorqurbonnazarov33 <baxtiyorqurbonnazarov33@gmail.com>
2025-07-18 10:16:07 +0500

    fix #3

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -349,6 +349,8 @@ export class ProductService {
         .leftJoinAndSelect('product.images', 'images')
         .leftJoinAndSelect('product.likes', 'likes')
         .leftJoinAndSelect('product.profile', 'profile')
+        .leftJoinAndSelect('product.region', 'region')
+        .leftJoinAndSelect('product.district', 'district')
         .where('product.id IN (:...ids)', { ids: localLikedProductIds ?? [] })
         .orderBy('images.order', 'ASC')
         .getMany();
