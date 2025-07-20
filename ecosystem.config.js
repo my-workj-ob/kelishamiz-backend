61afc70b671804a8eaff32be852edd329943035a
baxtiyorqurbonnazarov33 <baxtiyorqurbonnazarov33@gmail.com>
2025-07-20 15:21:29 +0500

    fix

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -12,7 +12,8 @@ module.exports = {
         PORT: 3030, // kerakli port
         DATABASE_URL:
           'postgres://neondb_owner:npg_cbWOXKt59Gmz@ep-cool-boat-a2p8ewnj-pooler.eu-central-1.aws.neon.tech:5432/neondb?sslmode=require',
-    
+        BLOB_READ_WRITE_TOKEN:
+          'vercel_blob_rw_Hl2g4VY0JzwRnHdy_88alBWTCk3BJPoDyuNMIP3E3FCm8CI',
       },
     },
   ],
