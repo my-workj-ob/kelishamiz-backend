9e3a6e2ba4ab80f064b1275ada0062c4f96913e2
baxtiyorqurbonnazarov33 <baxtiyorqurbonnazarov33@gmail.com>
2025-08-13 19:26:14 +0500

    fix

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,6 @@ import { join } from 'path';
 
 import { AppModule } from './app.module';
 import { ResponseInterceptor } from './common/interceptors/response.interceptor';
-import { IoAdapter } from '@nestjs/platform-socket.io';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -50,7 +49,7 @@ async function bootstrap() {
     express.static(join(__dirname, '../node_modules/swagger-ui-dist')),
   );
 
-  await app.listen(process.env.PORT || 3333);
+  await app.listen(process.env.PORT || 3030);
 }
 
 bootstrap();
