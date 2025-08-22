61941b53b8c530f95e2c361ec8b95bfd37ed62af
baxtiyorqurbonnazarov33 <baxtiyorqurbonnazarov33@gmail.com>
2025-08-22 18:31:30 +0500

    fix

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,11 +3,17 @@ module.exports = {
     {
       name: 'kelishamiz-backend',
       script: './dist/src/main.js',
-      instances: 1, // faqat bitta process
-      exec_mode: 'fork', // cluster o‘rniga fork
+      instances: 1,
       autorestart: true,
       watch: false,
       max_memory_restart: '200M',
+      env: {
+        NODE_ENV: 'production',
+        PORT: 3030,
+        DATABASE_URL: 'postgres://....',
+        BLOB_READ_WRITE_TOKEN:
+          'vercel_blob_rw_Hl2g4VY0JzwRnHdy_88alBWTCk3BJPoDyuNMIP3E3FCm8CI',
+      },
     },
   ],
 };
