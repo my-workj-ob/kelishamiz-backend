fd0a92e0c0e78ceced5ff138a7cf18e0f8da30bb
baxtiyorqurbonnazarov33 <baxtiyorqurbonnazarov33@gmail.com>
2025-08-22 18:26:47 +0500

    fix

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,14 +8,6 @@ module.exports = {
       autorestart: true,
       watch: false,
       max_memory_restart: '200M',
-      env: {
-        NODE_ENV: 'production',
-        PORT: 3030,
-        DATABASE_URL:
-          'postgres://neondb_owner:npg_cbWOXKt59Gmz@ep-cool-boat-a2p8ewnj-pooler.eu-central-1.aws.neon.tech:5432/neondb?sslmode=require',
-        BLOB_READ_WRITE_TOKEN:
-          'vercel_blob_rw_Hl2g4VY0JzwRnHdy_88alBWTCk3BJPoDyuNMIP3E3FCm8CI',
-      },
     },
   ],
 };
