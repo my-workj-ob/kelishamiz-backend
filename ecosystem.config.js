4943f709193def3ef5c9e1042093376ca060e354
baxtiyorqurbonnazarov33 <baxtiyorqurbonnazarov33@gmail.com>
2025-08-22 20:14:57 +0500

    fix

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,19 +1,14 @@
+// ecosystem.config.js
 module.exports = {
   apps: [
     {
       name: 'kelishamiz-backend',
       script: './dist/src/main.js',
       instances: 1,
+      exec_mode: 'fork',
       autorestart: true,
       watch: false,
       max_memory_restart: '200M',
-      env: {
-        NODE_ENV: 'production',
-        PORT: 3030,
-        DATABASE_URL: 'postgres://....',
-        BLOB_READ_WRITE_TOKEN:
-          'vercel_blob_rw_Hl2g4VY0JzwRnHdy_88alBWTCk3BJPoDyuNMIP3E3FCm8CI',
-      },
     },
   ],
 };
